541880303750deff4b178cd064c6546e1eaa06bd
Vicente Santos <vicentedpsantos@gmail.com>
2024-12-10 13:16:43 +0000

    First commit

---
+++ lib/faktur.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "faktur/version"
+
+module Faktur
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/faktur/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Faktur
+  VERSION = "0.1.0"
+end
